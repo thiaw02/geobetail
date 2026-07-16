b3201f498a0578c0c460237ae3724c2f3f3b6f3b
Lamine bara Thiaw <thiawlaminebara@gmail.com>
2026-07-16 13:20:26 +0000

    style: improve zones display; fix colliers API endpoint resolution

--- suivi_betail/animaux/urls_colliers.py
+++ suivi_betail/animaux/urls_colliers.py
@@ -1,23 +1,14 @@
 from django.urls import path
 from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
-from . import views
+from . import views_colliers
 
 app_name = 'colliers'
 
 urlpatterns = [
-    # Appairage par code
-    path('appairer/', views.appairer_collier, name='appairer'),
-    
-    # Appairage par QR code (décodé côté client)
-    path('appairer-qr/', views.appairer_collier_qr, name='appairer_qr'),
-    
-    # Liste des colliers de l'utilisateur
-    path('mes-colliers/', views.mes_colliers, name='mes_colliers'),
-    
-    # Dissocier un collier
-    path('<int:device_id>/dissocier/', views.dissocier_collier, name='dissocier'),
-    
-    # Génération QR code
-    path('<int:device_id>/qr-code/', views.qr_code_collier, name='qr_code'),
+    path('appairer/', views_colliers.appairer_collier, name='appairer'),
+    path('appairer-qr/', views_colliers.appairer_collier_qr, name='appairer_qr'),
+    path('mes-colliers/', views_colliers.mes_colliers, name='mes_colliers'),
+    path('<int:device_id>/dissocier/', views_colliers.dissocier_collier, name='dissocier'),
+    path('<int:device_id>/qr-code/', views_colliers.qr_code_collier, name='qr_code'),
 ]
